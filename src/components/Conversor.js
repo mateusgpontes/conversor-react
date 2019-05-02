67f6711278771a7e9fd8dded190e17e5208238c7
mateusgpontes <mateus.gpontes@hotmail.com>
2019-05-02 09:38:44 -0300

    create components and sort state

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Conversor extends Component {
+        state ={
+            moedaA_valor: "",
+            moedaB_valor:0
+        };
+        //bind vai referenciar quem vai ser o this, no caso o this vai ser o objeto dentro da função
+        //this.converter = this.converter.bind(this);
+
+
+    converter = () =>{
+        console.log(this.state)
+    }
+    
     render(){
         return(
             <>
-                <h2>USD para BRL</h2>
-                <input type="text"></input>
-                <input type="button" value="Converter"></input>
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange ={(event)=>{this.setState({moedaA_valor:event.target.value})}}>
+                </input>
+                <input type="button" value="Converter" onClick={this.converter}>
+                </input>
                 <h2>Valor convertido</h2>
             </>
         );
